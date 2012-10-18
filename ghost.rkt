e116ff8925f9653bd58999259860072e162b8b13
Kimball Germane <krgermane@yahoo.com>
2012-10-18 09:54:52 -0600

    Ghost updated to match new signature.

--- ghost.rkt
+++ ghost.rkt
@@ -17,8 +17,11 @@
                                          (file->lines #:mode 'text "/usr/share/dict/words")))])
                  (set-add dict word)))
   
+   (define (letters->string letters)
+     (list->string (reverse letters)))
+  
   (define (forms-word? letters)
-    (set-member? dict (list->string (reverse letters))))
+    (set-member? dict (string-downcase (letters->string letters))))
   
   (define (next-turn i n)
     (remainder (add1 i) n))
@@ -27,68 +30,107 @@
     (remainder (+ (sub1 i) n) n))
   
   (struct middle (letters turn n) #:transparent)
-  (struct challenged (letters er ee) #:transparent)
-  (struct defending (letters er ee) #:transparent)
-  (struct end (winner) #:transparent) ; reward calling people out
+  (struct challenged (letters challenger challenged) #:transparent)
+  (struct defending (letters challenger challenged) #:transparent)
+  (struct end (loser) #:transparent)
   
   (struct challenge () #:transparent)
   (struct declare () #:transparent)
   (struct forfeit () #:transparent)
   (struct add (letter) #:transparent)
   
-  (define moves (list* (challenge)
-                       (declare)
-                       (forfeit)
-                       (for/list ([c (in-list (string->list "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))])
-                         (add c))))
+  (define add-letters (for/list ([c (in-list (string->list "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))])
+                        (add c)))
   
-  (define initial-state (middle empty 0 2))
+  (define (make-initial-state n)
+    (and (> n 1) (middle empty 0 n)))
+  
+  (define available
+    (match-lambda*
+      [(list (middle letters p _) p)
+       (let* ([moves add-letters]
+              [moves (if (> (length letters) 0) (cons (challenge) moves) moves)]
+              [moves (if (> (length letters) 2) (cons (declare) moves) moves)])
+         moves)]
+      [(list (challenged _ _ p) p)
+       (cons (forfeit) add-letters)]
+      [(list (defending _ _ p) p)
+       (list* (declare) (forfeit) add-letters)]
+      [(list _ _)
+       empty]))
   
   (define next
     (match-lambda*
       ;; If there are letters, challenge.
-      [(list (middle letters turn n) (challenge))
-       (and (> (length letters) 0) (challenged letters turn (last-turn turn n)))]
+      [(list (middle letters p n) p (challenge))
+       (and (> (length letters) 0) (challenged letters p (last-turn p n)))]
       ;; If there are more than two letters, declare.
-      [(list (middle letters turn n) (declare))
+      [(list (middle letters p n) p (declare))
        (and (> (length letters) 2) (if (forms-word? letters)
-                                       (end n)
-                                       (end (last-turn turn n))))]
+                                       (end (last-turn p n))
+                                       (end p)))]
       ;; If adding letters, add a letter.
-      [(list (middle letters turn n) (add c))
-       (middle (cons c letters) (next-turn turn n) n)]
-      ;; If challenged, add a letter
-      [(list (challenged letters er ee) (add c))
-       (defending (cons c letters) er ee)]
+      [(list (middle letters p n) p (add c))
+       (middle (cons c letters) (next-turn p n) n)]
+      ;; Challenged person begins to finish the word
+      [(list (challenged letters er p) p (add c))
+       (defending (cons c letters) er p)]
       ;; challenged [person] forfeits
-      [(list (challenged letters er ee) (forfeit))
-       (end er)]
+      [(list (challenged letters er p) p (forfeit))
+       (end p)]
       ;; defender adds letter
-      [(list (defending letters er ee) (add c))
-       (defending (cons c letters) er ee)]
+      [(list (defending letters er p) p (add c))
+       (defending (cons c letters) er p)]
       ;; defender forfeits
-      [(list (defending letters er ee) (forfeit))
-       (end er)]
+      [(list (defending letters er p) p (forfeit))
+       (end p)]
       ;; defender finishes word
-      [(list (defending letters er ee) (declare))
+      [(list (defending letters er p) p (declare))
        (if (forms-word? letters)
-           (end ee)
-           (end er))]
+           (end er)
+           (end p))]
       ;; Or you can't do anything
       [_
        #f]))
   
-  (define winner
-    (match-lambda
-      [(end w) w]
-      [_ #f]))
+  (define score
+    (match-lambda*
+      [(list (end p) p)
+       -1]
+      [(list _ _)
+       0]))
   
-  (define player
-    (match-lambda
-      [(middle _ turn _) turn]
-      [(challenged _ _ ee) ee]
-      [(defending _ _ ee) ee]
-      [(end w) w])))
+  (define render
+    (match-lambda*
+      [(list (middle letters _ _) _)
+       (if (> (length letters) 0)
+           (letters->string letters)
+           "No letters have been added yet.")]
+      [(list (challenged letters er ee) er)
+       (format "You challenged player ~a on ~a." ee (letters->string letters))]
+      [(list (challenged letters er ee) ee)
+       (format "You were challenged by player ~a on ~a." er (letters->string letters))]
+      [(list (challenged letters er ee) _)
+       (format "Player ~a challenged player ~a on ~a." er ee (letters->string letters))]
+      [(list (defending letters er ee) ee)
+       (letters->string letters)]
+      [(list (defending letters _ ee) _)
+       (format "Player ~a is attempting to finish ~a." ee (letters->string letters))]
+      [(list (end p) p)
+       "You were eliminated."]
+      [(list (end p) _)
+       (format "Player ~a was eliminated." p)]
+      [(list (add c) _)
+       (format "add ~a" c)]
+      [(list (challenge) _)
+       "challenge"]
+      [(list (declare) _)
+       "declare current state to be a word"]
+      [(list (forfeit) _)
+       "give up"]
+      [(list _ _)
+       "<RENDER NOT IMPLEMENTED>"])))
 
 (module+ main
-  (invoke-unit/infer (link ghost@ arroy@)))
+  (define-values/invoke-unit/infer (export arroy^) (link ghost@ arroy@))
+  (play 2))
